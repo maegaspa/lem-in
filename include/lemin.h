cb23a6450db881255038fb1dc33f4c185161356c
Clement Garrot <cgarrot@z4r1p4.le-101.fr>
2019-10-07 13:36:37 +0200

    Add all feature

---
+++ include/lemin.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                          LE - /            */
+/*                                                              /             */
+/*   lemin.h                                          .::    .:/ .      .::   */
+/*                                                 +:+:+   +:    +:  +:+:+    */
+/*   By: cgarrot <marvin@le-101.fr>                 +:+   +:    +:    +:+     */
+/*                                                 #+#   #+    #+    #+#      */
+/*   Created: 2019/10/07 13:30:44 by cgarrot      #+#   ##    ##    #+#       */
+/*   Updated: 2019/10/07 13:35:32 by cgarrot     ###    #+. /#+    ###.fr     */
+/*                                                         /                  */
+/*                                                        /                   */
+/* ************************************************************************** */
+
+#ifndef LEMIN_H
+# define LEMIN_H
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <sys/types.h>
+# include <sys/stat.h>
+# include <fcntl.h>
+# include "../libft/includes/ft_printf.h"
+
+#endif
