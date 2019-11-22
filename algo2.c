37fb07af0c392cd7e3dc95d1434a16a96c344b44
Clement Garrot <cgarrot@z4r2p3.le-101.fr>
2019-11-22 15:52:46 +0100

    Systeme of first path and second path is OK

--- algo2.c
+++ algo2.c
@@ -19,21 +19,31 @@ int 	*ft_intdup(int *dst, int *src, unsigned int size)
 
 int 	**clear_path(int	**path, int nb_path, int nb_fourm, int start, int end, int *file)
 {
-	int 	**new_path_copy;
-	int 	*new_file;
-	int i;
-	int j;
-	int	count;
-	int	nb_new_path;
-	int error;
+	int 	**first_path;
+	int 	**second_path;
+	int 	*new_file_first;
+	int 	*new_file_second;
+	int 	i;
+	int 	j;
+	int		count;
+	int		nb_new_path_first;
+	int		nb_new_path_second;
+	int 	error;
+	int 	occurrence;
 
 	i = 1;
-	if (!(new_file = malloc(sizeof(int) * nb_path)))
+	occurrence = 0;
+	if (!(new_file_first = malloc(sizeof(int) * nb_path)))
 		return (0);
-	if (!(new_path_copy = malloc(sizeof(int*) * nb_path)))
+	if (!(new_file_second = malloc(sizeof(int) * nb_path)))
 		return (0);
-	nb_new_path = 1;
-	new_path_copy[0] = ft_intdup(new_path_copy[0], path[0], file[0]);
+	if (!(first_path = malloc(sizeof(int*) * nb_path)))
+		return (0);
+	if (!(second_path = malloc(sizeof(int*) * nb_path)))
+		return (0);
+	nb_new_path_first = 1;
+	nb_new_path_second = 0;
+	first_path[0] = ft_intdup(first_path[0], path[0], file[0]);
 	while (i < nb_path)
 	{
 		j = -1;
@@ -42,31 +52,66 @@ int 	**clear_path(int	**path, int nb_path, int nb_fourm, int start, int end, int
 		{
 			count = -1;
 			if ((path[i][0] == start) && (path[i][j] != end))
-				while (++count < nb_new_path && j < file[i] - 1)
-					if (path[i][j] == new_path_copy[count][j])
+				while (++count < nb_new_path_first && j < file[i] - 1)
+					if (path[i][j] == first_path[count][j])
 					{
+						//printf("%d-%d\n", path[i][j], first_path[count][j]);
+						occurrence = count;
+						//printf("%d\n", path[i][j]);
+						//printf("%d||%d||%d\n", count, i, error);
 						error++;
+						//printf("%d||%d||%d\n", count, i, error);
 						break;
 					}
 		}
 		if (error == 1)
 		{
-			new_path_copy[nb_new_path] = ft_intdup(new_path_copy[nb_new_path], path[i], file[i]);
-			new_file[nb_new_path] = file[i];
-			nb_new_path++;
+			//printf("*%d*\n", occurrence);
+			first_path[nb_new_path_first] = ft_intdup(first_path[nb_new_path_first], path[i], file[i]);
+			new_file_first[nb_new_path_first] = file[i];
+			nb_new_path_first++;
 		}
-		else if (file[nb_new_path - 1] > file[i])
+		//double while comme pour trier du plus petit au plus grand si jamais grosse suite de grand chemain surment a fix soon alors
+		//changer le -1 de par un search de salle commune pour cibler la bonne ordre de first path
+		else
 		{
-			ft_bzero(&new_path_copy[nb_new_path - 1], file[count - 1]);
-			new_path_copy[nb_new_path - 1] = path[i];
-			new_file[nb_new_path - 1] = file[i];
+			//printf("%d/-*-/%d\n", file[occurrence], file[i]);
+			if ((file[occurrence] > file[i]))
+			{
+				//printf("Slt\n");
+				//printf("[%d-%d-%d-%d-%d-%d\n", first_path[0][0],first_path[0][1], first_path[0][2], first_path[0][3], first_path[0][4], first_path[0][5]);
+				//printf("%d//%d]\n", file[occurrence], occurrence);
+				second_path[nb_new_path_second] = ft_intdup(second_path[nb_new_path_second], first_path[occurrence], file[occurrence]);
+				new_file_second[nb_new_path_second] = file[occurrence];
+				ft_bzero(&first_path[occurrence], file[occurrence]);
+				first_path[occurrence] = path[i];
+				new_file_first[occurrence] = file[i];
+				nb_new_path_second++;
+			}
+			else if (new_file_first[occurrence] > file[i])
+			{
+				second_path[nb_new_path_second] = ft_intdup(second_path[nb_new_path_second], first_path[occurrence], new_file_first[occurrence]);
+				new_file_second[nb_new_path_second] = file[occurrence];
+				ft_bzero(&first_path[occurrence], file[occurrence]);
+				first_path[occurrence] = path[i];
+				new_file_first[occurrence] = file[i];
+				nb_new_path_second++;
+			}
+			else
+			{
+				second_path[nb_new_path_second] = ft_intdup(second_path[nb_new_path_second], path[i], file[i]);
+				new_file_second[nb_new_path_second] = file[i];
+				nb_new_path_second++;
+			}
 		}
 		i++;
 	}
-	return (new_path_copy);
+	printf("%d-%d-%d-%d-%d\n", second_path[1][0], second_path[1][1], second_path[1][2], second_path[1][3], second_path[1][4]);
+	printf("%d-%d-%d-%d-%d-%d\n\n", second_path[0][0], second_path[0][1], second_path[0][2], second_path[0][3], second_path[0][4], second_path[0][5]);
+	return (first_path);
 }
 
-int 	path_line_ant(int	**path, int start, int end, int nb_ant, int nb_path, int *file)
+int 	path_line_ant(int	**first_path, int **second_path, int start, int end, int nb_ant, int nb_first_path, int nb_second_path, int *first_file, int *second_file)
 {
 	int 	i;
 	int 	j;
@@ -91,22 +136,22 @@ int 	path_line_ant(int	**path, int start, int end, int nb_ant, int nb_path, int 
 	while (ant_finish < nb_ant)
 	{
 		i = 0;
-		while (i < nb_path)
+		while (i < nb_first_path)
 		{
 			j = 0;
 			k = 0;
-			while (j < (nb_path + nb_ant_cross))
+			while (j < (nb_first_path + nb_ant_cross))
 			{
 				l = 0;
 				m = 0;
-				printf("[%d] == [%d]\n", path[k][l], status_ant[m]);
-				while (path[k][l] == status_ant[m])
+				//printf("[%d] == [%d]\n", path[k][l], status_ant[m]);
+				while (first_path[k][l] == status_ant[m])
 				{
 					//printf("[%d]-[%d]\n", m, l);
-					printf("[%d] == [%d]\n", path[k][l], status_ant[m]);
+					//printf("[%d] == [%d]\n", path[k][l], status_ant[m]);
 					l++;
 				}
-				status_ant[m] = path[k][l];
+				status_ant[m] = first_path[k][l];
 				j++;
 			}
 			i++;
@@ -121,30 +166,38 @@ int 	main(void)
 	int **path;
 	char **copy_path;
 	int **new_path;
-	char **new_copy_path;
-	int file[3] = {5, 4, 3};
+	int **new_copy_path;
+	int file[4] = {6, 3, 5, 4};
 	int file2[2] = {3, 4};
 
-	if (!(path = malloc(sizeof(int*) * 3)))
+	if (!(path = malloc(sizeof(int*) * 4)))
 		return (0);
-	if (!(path[2] = malloc(sizeof(int) * 3)))
-		return (0);
-	path[2][0] = 1;
-	path[2][1] = 2;
-	path[2][2] = 7;
-	if (!(path[1] = malloc(sizeof(int) * 4)))
+	if (!(path[1] = malloc(sizeof(int) * 3)))
 		return (0);
 	path[1][0] = 1;
-	path[1][1] = 3;
-	path[1][2] = 5;
-	path[1][3] = 7;
-	if (!(path[0] = malloc(sizeof(int) * 5)))
+	path[1][1] = 2;
+	path[1][2] = 7;
+	if (!(path[3] = malloc(sizeof(int) * 4)))
+		return (0);
+	path[3][0] = 1;
+	path[3][1] = 3;
+	path[3][2] = 5;
+	path[3][3] = 7;
+	if (!(path[2] = malloc(sizeof(int) * 5)))
+		return (0);
+	path[2][0] = 1;
+	path[2][1] = 4;
+	path[2][2] = 5;
+	path[2][3] = 6;
+	path[2][4] = 7;
+	if (!(path[0] = malloc(sizeof(int) * 6)))
 		return (0);
 	path[0][0] = 1;
-	path[0][1] = 4;
-	path[0][2] = 5;
-	path[0][3] = 6;
-	path[0][4] = 7;
+	path[0][1] = 2;
+	path[0][2] = 3;
+	path[0][3] = 5;
+	path[0][4] = 6;
+	path[0][5] = 7;
 
 	if (!(copy_path = malloc(sizeof(char*) * 4)))
 		return (0);
@@ -171,28 +224,26 @@ int 	main(void)
 	copy_path[0][5] = '\0';
 	copy_path[3] = NULL;
 
-	if (!(new_copy_path = malloc(sizeof(char*) * 3)))
+	if (!(new_copy_path = malloc(sizeof(int*) * 2)))
 		return (0);
-	if (!(new_copy_path[0] = malloc(sizeof(char) * 4)))
+	if (!(new_copy_path[0] = malloc(sizeof(int) * 3)))
 		return (0);
 	new_copy_path[0][0] = '1';
 	new_copy_path[0][1] = '2';
 	new_copy_path[0][2] = '7';
-	new_copy_path[0][3] = '\0';
-	if (!(new_copy_path[1] = malloc(sizeof(char) * 5)))
+	if (!(new_copy_path[1] = malloc(sizeof(int) * 4)))
 		return (0);
 	new_copy_path[1][0] = '1';
 	new_copy_path[1][1] = '3';
 	new_copy_path[1][2] = '5';
 	new_copy_path[1][3] = '7';
-	new_copy_path[1][4] = '\0';
 
 
 
-	new_path = clear_path(path, 3, 10, 1, 7, file);
-	printf("%d-%d-%d\n", new_path[1][0], new_path[1][1], new_path[1][2]);
+	new_path = clear_path(path, 4, 10, 1, 7, file);
+	printf("%d-%d-%d\n", new_path[0][0], new_path[0][1], new_path[0][2]);
 	//printf("%d-%d-%d-%d-%d\n", new_path[1][0], new_path[1][1], new_path[1][2], new_path[1][3], new_path[1][4]);
-	printf("%d-%d-%d-%d\n", new_path[0][0], new_path[0][1], new_path[0][2], new_path[0][3]);
-	path_line_ant(new_path, 1, 6, 10, 2, file);
+	printf("%d-%d-%d-%d\n", new_path[1][0], new_path[1][1], new_path[1][2], new_path[1][3]);
+	path_line_ant(new_path, new_copy_path, 1, 6, 10, 2, 1, file, file2);
 	return (0);
 }
